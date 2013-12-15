c977dca510332b69be0b0fb21eb0acca9777fc6b
Andrey Bayandin <dontusecascade@gmail.com>
2013-12-15 23:47:48 +0600

    need add fonts

--- html/js/all.js
+++ html/js/all.js
@@ -1,1 +1,32 @@
-var glb = {};
+/* map */
+$(function(){
+    $block = $('.js-map');
+
+    if($block.length === 0) {
+        return;
+    }
+
+    var $points = $('.b-map__elem',$block);
+
+    $points.click(function(e){
+        e.preventDefault();
+        $points.removeClass('b-map__elem_cur');
+        $(this).addClass('b-map__elem_cur');
+    });
+
+
+});
+/*map*/
+
+/* navigation */
+$(function(){
+
+});
+/* navigation */
+
+
+/* carousel */
+$(function(){
+
+});
+/* carousel */
